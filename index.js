503e6ae39cb78ba8039ff58ea99876ad2a9b8997
Titia de Castro <decastrotitia@gmail.com>
2022-05-19 14:02:51 +0800

    add spotlight functions

---
+++ index.js
@@ -0,0 +1,14 @@
+exports.addItems = () => {
+  console.log('Called electron-spotlight-win addItems')
+  return true
+}
+
+exports.removeItems = () => {
+  console.log('Called electron-spotlight-win removeItems')
+  return true
+}
+
+exports.removeAllItems = () => {
+  console.log('Called electron-spotlight-win removeAllItems')
+  return true
+}
